56452ec05bfebcb68b2c7cb49c7ed49372a67390
Yuichi Ito <yito86@gmail.com>
2019-06-17 16:42:12 +0900

    a
    a

--- django/install_ubuntu.sh
+++ django/install_ubuntu.sh
@@ -1,5 +1,11 @@
-sudo apt install python3-pip
-sudo apt install ipmitool
-
+sudo apt-get -y install python3-pip
+sudo apt-get -y install ipmitool
+sudo apt-get -y install ansible
+sudo apt-get -y install sshpass
 pip3 install requests paramiko django whitenoise
 
+mkdir ~/.ssh
+cd ~/.ssh
+ssh-keygen -t rsa -N "" -f ~/.ssh/id_rsa
+sudo su
+cp id_rsa* /root/.ssh/
---
+++ django/start.sh
@@ -0,0 +1,3 @@
+screen -S django
+screen -r django
+sudo python3 manage.py runserver 0.0.0.0:80
